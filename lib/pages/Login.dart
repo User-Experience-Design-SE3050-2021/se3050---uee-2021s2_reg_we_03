5d0f53b2cfb574a3252c25128594cd1a394ee971
udithajanadara <developer1@chanodil.com>
2021-07-26 17:25:06 +0530

    login page UI

---
+++ lib/pages/Login.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 400,
+                child: Image(image: AssetImage('assets/images/Logo.png'),
+                  fit:BoxFit.contain,
+                ),
+              ),
+              Container(
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                            prefixIcon: Icon(Icons.email)
+                          ),
+                        ) ,
+
+                      ),
+
+                      Container(
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                              labelText: 'Password',
+                              prefixIcon: Icon(Icons.lock)
+                          ),
+                        ) ,
+
+                      ),
+
+                      SizedBox(height: 20),
+                      FlatButton(
+                        padding: EdgeInsets.fromLTRB(90, 15, 90, 15),
+                        color: Colors.red,
+                        splashColor: Colors.black12,
+
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0)
+                        ),
+                        onPressed: (){
+                        },
+                        child: Text(
+                            "Login",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        )
+    );
+  }
+}
